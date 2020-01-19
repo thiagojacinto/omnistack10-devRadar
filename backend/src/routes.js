348efda9f676cf765ff7f3ecc8d753c13342ee5a
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-01-19 15:30:35 -0300

    update route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -11,6 +11,8 @@ routes.post('/v1/devs', DeveloperController.store);
 routes.get('/v1/devs', DeveloperController.index);
 // Removes a developer by its 'github_username'
 routes.delete('/v1/removedev', DeveloperController.destroy);
+// Updates developer info by id:
+routes.put('/v1/update/:id', DeveloperController.update);
 
 // Search a specific developer within radius 10 km:
 routes.get('/v1/searchdev', SearchController.index);
