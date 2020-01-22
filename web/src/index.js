f708cce70770f2577bb59d64f50c85411b7fa507
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2020-01-21 22:19:11 -0300

    bootstrap importing config

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Bootstrap4
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+);
